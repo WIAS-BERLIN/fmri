b1e9dbed0b44bcb890414188acc8d61ddcd5d7ef
stat <stat@090e7faf-2252-4674-a6b5-969773fc8662>
2005-09-19 09:24:45 +0000

    karsten: consider bias for autocorrelation according to Worsley2002. this is computational very unefficient, but seems to do its job. further tests useful. unclear, how to make it considerable faster, since formulation in R is very efficient.
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/fmri-svn/trunk@19 090e7faf-2252-4674-a6b5-969773fc8662

--- R/fmri.R
+++ R/fmri.R
@@ -211,6 +211,7 @@ create.stimulus <- function(scans=1 ,onsets=c(1) ,length=1, rt=3, mean=TRUE) {
   }
 
   hrf <- convolve(stimulus,mygamma(scans:1, 6, 12, 0.9/rt, 0.9/rt, 0.35))
+  dim(hrf) <- c(scans,1)
   
   if (mean) {
     hrf - mean(hrf)
@@ -278,6 +279,10 @@ calculate.lm <- function(ttt,z,actype="smooth",hmax=4,qlambda=0.96,vtype="var") 
   arfactor <- rep(0,length=prod(dy[1:3]))
   variance <- rep(0,length=prod(dy[1:3]))
 
+  # calculate matrix R for bias correction in correlation coefficient
+  # estimate
+  R <- diag(1,dy[4]) - u %*% t(u)
+
   # calculate the paramters and residuals for all voxels
   beta <- ttt %*% u %*% lambda1 %*% vt
   residuals <- ttt - beta %*% t(z)
@@ -297,11 +302,30 @@ calculate.lm <- function(ttt,z,actype="smooth",hmax=4,qlambda=0.96,vtype="var") 
         progress = progress + 10
       }
       # calculate the Koeff of ACR(1) time series model
-      if (sum(abs(residuals[i,]))) {
-        arfactor[i] <- residuals[i,2:dim(z)[1]] %*% residuals[i,1:(dim(z)[1]-1)] / residuals[i,] %*% residuals[i,]
+      a0 <- residuals[i,] %*% residuals[i,]
+      a1 <- residuals[i,-1] %*% residuals[i,-dim(z)[1]]
+      m00 <- dy[4] - dim(z)[2]
+      m01 <- 0
+      for (k in 1:dy[4]) {m01 <- m01 + sum(R[k,-1]*R[k,-dy[4]])}
+      m10 <- m01
+      m11 <- 0
+      for (k in 1:(dy[4]-1)) {m11 <- m11 + sum(R[k+1,-dy[4]]*R[k,-1] + R[k+1,-1]*R[k,-dy[4]]) }
+      Minv <- matrix(c(m11,-m01,-m10,m00),2,2)/(m00*m11-m01*m10) # inverse
+      an <- Minv %*% c(a0,a1)
+      if (an[1] != 0) {
+        arfactor[i] <- an[2]/an[1]
       } else {
-        arfactor[i] <- 0 # avoid NaN
+        arfactor[i] <- 0
       }
+
+      ### this method does have a bias!
+#      if (sum(abs(residuals[i,]))) {
+#        arfactor[i] <- residuals[i,2:dim(z)[1]] %*% residuals[i,1:(dim(z)[1]-1)] / residuals[i,] %*% residuals[i,]
+#      } else {
+#        arfactor[i] <- 0 # avoid NaN
+#      }
+      ### leave it for historical reasons
+      
     }
     cat("\n")
     cat("calculate.lm: finished\n")
@@ -483,29 +507,29 @@ plot.fmri <- function(signal, mask, anatomic, x=-1, y=-1, z=-1, zlim=0, device="
 }
 
 
-pvalue <- function(z,i,j,k,rx,ry,rz,field="norm",nu=4) {
+pvalue <- function(z,i,j,k,rx,ry,rz,type="norm",df=4,dim=3) {
   rho0 <- 0
   rho1 <- 0
   rho2 <- 0
   rho3 <- 0
   
-  if (field == "norm") {
+  if (type == "norm") {
     rho0 <- 1 - pnorm(z)
     rho1 <- (4 * log(2))^.5 / (2 * pi) * exp(-z*z/2)
     rho2 <- (4 * log(2)) / (2 * pi)^1.5 * exp(-z*z/2) * z
     rho3 <- (4 * log(2))^1.5 / (2 * pi)^2 * exp(-z*z/2) * (z*z -1)
   }
-  if (field == "t") {
-    rho0 <- 1 - pt(z,nu)
-    rho1 <- (4 * log(2))^.5 / (2 * pi) * (1+z*z/nu)^(-0.5*(nu-1))
-    rho2 <- (4 * log(2)) / (2 * pi)^1.5 * gamma((nu+1)/2)/gamma(nu/2)/(nu/2)^0.5 * (1+z*z/nu)^(-0.5*(nu-1)) * z
-    rho3 <- (4 * log(2))^1.5 / (2 * pi)^2 * (1+z*z/nu)^(-0.5*(nu-1)) * ((nu-1)/nu*z*z-1)
+  if (type == "t") {
+    rho0 <- 1 - pt(z,df)
+    rho1 <- (4 * log(2))^.5 / (2 * pi) * (1+z*z/df)^(-0.5*(df-1))
+    rho2 <- (4 * log(2)) / (2 * pi)^1.5 * gamma((df+1)/2)/gamma(df/2)/(df/2)^0.5 * (1+z*z/df)^(-0.5*(df-1)) * z
+    rho3 <- (4 * log(2))^1.5 / (2 * pi)^2 * (1+z*z/df)^(-0.5*(df-1)) * ((df-1)/df*z*z-1)
   }
-  if (field == "chisq") {
-    rho0 <- 1 - pchisq(z,nu)
-    rho1 <- (4 * log(2))^.5 / (2 * pi)^.5 * chihelp(z,nu,1)
-    rho2 <- (4 * log(2)) / (2 * pi) * chihelp(z,nu,2) * (z-nu+1)
-    rho3 <- (4 * log(2))^1.5 / (2 * pi)^1.5 * chihelp(z,nu,3) * (z*z - (2*nu-1)*z + (nu-1) * (nu-2))    
+  if (type == "chisq") {
+    rho0 <- 1 - pchisq(z,df)
+    rho1 <- (4 * log(2))^.5 / (2 * pi)^.5 * chihelp(z,df,1)
+    rho2 <- (4 * log(2)) / (2 * pi) * chihelp(z,df,2) * (z-df+1)
+    rho3 <- (4 * log(2))^1.5 / (2 * pi)^1.5 * chihelp(z,df,3) * (z*z - (2*df-1)*z + (df-1) * (df-2))    
   }
   
   r0 <- 1
@@ -513,35 +537,39 @@ pvalue <- function(z,i,j,k,rx,ry,rz,field="norm",nu=4) {
   r2 <- (i-1)*(j-1)*rx*ry + (j-1)*(k-1)*ry*rz +(i-1)*(k-1)*rx*rz
   r3 <- (i-1)*(j-1)*(k-1)*rx*ry*rz
   
-  rho0 * r0 + rho1 * r1 + rho2 * r2 + rho3 * r3
+  if (dim == 2) {
+    rho0 * r0 + rho1 * r1 + rho2 * r2
+  } else {
+    rho0 * r0 + rho1 * r1 + rho2 * r2 + rho3 * r3
+  }
 }
 
-chihelp <- function(z,nu,d) {
-  z^((nu-d)/2) * exp(-z/2) / 2^((nu-2)/2) / gamma(nu/2)
+chihelp <- function(z,df,d) {
+  z^((df-d)/2) * exp(-z/2) / 2^((df-2)/2) / gamma(df/2)
 }
 
 # the following function is possibly obsolete since we do not
 # implement an algorithm using the derivative 
-pvalueprime <- function(z,i,j,k,rx,ry,rz,field="norm",nu=4) {
+pvalueprime <- function(z,i,j,k,rx,ry,rz,type="norm",df=4,dim=3) {
   rho0prime <- 0
   rho1prime <- 0
   rho2prime <- 0
   rho3prime <- 0
   
-  if (field == "norm") {
+  if (type == "norm") {
     rho0prime <- dnorm(z)
     rho1prime <- -(4 * log(2))^.5 / (2 * pi) * exp(-z*z/2) * z
     rho2prime <- -(4 * log(2)) / (2 * pi)^1.5  * exp(-z*z/2) * (z*z -1)
     rho3prime <- -(4 * log(2))^1.5 / (2 * pi)^2 * exp(-z*z/2) * (z*z*z - z)
   }
-  if (field == "t") {
-    rho0 <- 1 - pt(z,nu)
+  if (type == "t") {
+    rho0 <- 1 - pt(z,df)
     rho1 <- 0
     rho2 <- 0
     rho3 <- 0
   }
-  if (field == "chisq") {
-    rho0 <- 1 - pchisq(z,nu)
+  if (type == "chisq") {
+    rho0 <- 1 - pchisq(z,df)
     rho1 <- 0
     rho2 <- 0
     rho3 <- 0    
@@ -552,18 +580,22 @@ pvalueprime <- function(z,i,j,k,rx,ry,rz,field="norm",nu=4) {
   r2 <- (i-1)*(j-1)*rx*ry + (j-1)*(k-1)*ry*rz +(i-1)*(k-1)*rx*rz
   r3 <- (i-1)*(j-1)*(k-1)*rx*ry*rz
 
-  rho0prime * r0 + rho1prime * r1 + rho2prime * r2 + rho3prime * r3
+  if (dim == 2) {
+    rho0prime * r0 + rho1prime * r1 + rho2prime * r2
+  } else {
+    rho0prime * r0 + rho1prime * r1 + rho2prime * r2 + rho3prime * r3
+  }
 }
 # end of obsolete function
 
 
 resel <- function(voxeld, hmax, hv=0.919) hv * voxeld / hmax
 
-threshold <- function(p,i,j,k,rx,ry,rz,field="norm",nu=4) {
+threshold <- function(p,i,j,k,rx,ry,rz,type="norm",df=4,dim=3) {
   f <- function(x,par){
-    abs(pvalue(x,par$i,par$j,par$k,par$rx,par$ry,par$rz,par$field,par$nu)-par$p)
+    abs(pvalue(x,par$i,par$j,par$k,par$rx,par$ry,par$rz,par$type,par$df,par$dim)-par$p)
   }
-  optimize(f=f,lower=2,upper=10,tol=1e-6,par=list(p=p,i=i,j=j,k=k,rx=rx,ry=ry,rz=rz,field=field,nu=nu))$minimum
+  optimize(f=f,lower=2,upper=10,tol=1e-6,par=list(p=p,i=i,j=j,k=k,rx=rx,ry=ry,rz=rz,type=type,df=df,dim=dim))$minimum
 }
 
 gkernsm <- function(y,h=1) {
