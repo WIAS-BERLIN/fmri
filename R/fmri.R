aec86fea7e06c952ba3861956f7de45ca3c56082
stat <stat@090e7faf-2252-4674-a6b5-969773fc8662>
2005-09-21 12:40:47 +0000

    karsten/joerg: to make it run faster: 1. change create.arcorrection and fill a vector instead of creating an array and fill this 2.  change smoothness and use table in sysdata.rda (contains cor2bw dim=c(400,2) at the moment, where the first element of second component is the bandwidth and the second the corresponding spatial correlation. Note that this table has some minor variance due to the random field basing its calculation) to give smoothness for given correlation.
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/fmri-svn/trunk@22 090e7faf-2252-4674-a6b5-969773fc8662

--- R/fmri.R
+++ R/fmri.R
@@ -242,22 +242,17 @@ create.designmatrix <- function(hrf, order=0) {
 }
 
 create.arcorrection <- function(scans, rho=0) {
-  a <- array(0,dim=c(scans,scans))
+  rho0 <- 1/sqrt(1-rho^2)
+  a <- numeric(scans*scans)
   
-  a[1,1] <- 1
-
-  rho0 <- sqrt(1-rho^2)
-  rho1 <- -rho/rho0
-  rho2 <- 1/rho0
+  a[1] <- 1
+  ind <- (2:scans) *(scans+1) - 2*scans
+  a[ind] <- -rho*rho0
+  a[ind+scans] <- rho0
+  dim(a) <- c(scans,scans)
   
-  for (i in 2:scans) {
-    a[i,i-1] <- rho1
-    a[i,i] <- rho2
-  }
-
   a
 }
-
 
 calculate.lm <- function(ttt,z,actype="smooth",hmax=4,qlambda=0.96,vtype="var") {
   require(aws)
@@ -355,17 +350,15 @@ calculate.lm <- function(ttt,z,actype="smooth",hmax=4,qlambda=0.96,vtype="var") 
         a <- create.arcorrection(dy[4],arfactor[i]) # create prewhitening matrix
         zprime <- a %*% z
         svdresult <- svd(zprime) # calc SVD of prewhitened design
-        u <- svdresult$u 
         v <- svdresult$v
         vt <- t(v)
-        lambda1 <- diag(1/svdresult$d)
-        lambda2 <- diag(1/svdresult$d^2)
-        sigma <- v %*% lambda2 %*% vt # sigma * <estimate of varince> of prewhitened noise is variance of parameter estimate
+        sigma <- v %*% diag(1/svdresult$d^2) %*% vt # sigma * <estimate of varince> of prewhitened noise is variance of parameter estimate
         tttprime <- ttt[i,] %*% t(a)
-        beta[i,] <- tttprime %*% u %*% lambda1 %*% vt # estimate parameter
+        beta[i,] <- tttprime %*% svdresult$u %*% diag(1/svdresult$d) %*% vt # estimate parameter
         residuals[i,] <- tttprime - beta[i,] %*% t(zprime) # calculate residuals
-        variance[i] <- residuals[i,] %*% residuals[i,] / (dim(z)[1]-dim(z)[2]) * sigma[1,1] # variance estimate, add for more paramters if needed
+        variance[i] <- residuals[i,] %*% residuals[i,] * sigma[1,1] # variance estimate, add for more paramters if needed
       }
+      variance <- variance / (dim(z)[1]-dim(z)[2])
       cat("\n")
       cat("calculate.lm: finished\n")
     } else {
@@ -386,7 +379,7 @@ calculate.lm <- function(ttt,z,actype="smooth",hmax=4,qlambda=0.96,vtype="var") 
 
   cat("calculate.lm: exiting function\n")
   
-  list(beta=beta,var=variance,arfactor=arfactor)
+  list(beta=beta,var=variance,res=residuals,arfactor=arfactor)
 }
 
 perform.aws <- function(beta,variance,hmax=4,hinit=1,weights=c(1,1,1),vweights=NULL,qlambda=1) {
@@ -589,7 +582,7 @@ pvalueprime <- function(z,i,j,k,rx,ry,rz,type="norm",df=4,dim=3) {
 # end of obsolete function
 
 
-resel <- function(voxeld, hmax, hv=0.919) hv * voxeld / hmax
+resel <- function(voxeld, hmax, hv=0.95) hv * voxeld / hmax # hv=0.919 for larger bandwidths than typically fmri
 
 threshold <- function(p,i,j,k,rx,ry,rz,type="norm",df=4,dim=3) {
   f <- function(x,par){
@@ -640,23 +633,18 @@ correlation <- function(res,mask) {
   }
 }
 
-smoothness <- function(cor, dim, h = 0, step = 0.1) {
-  field <- array(rnorm(prod(dim)),dim)
-  repeat {
-    h <- h + step
-    z <- gkernsm(field, h)$gkernsm;
-    corg <- mean(z[,-1,]*z[,-dim[2],])/var(z)
-#    cat(corg,h,"\n")
-    if (corg > cor) break
+smoothness <- function(cor) {
+  for (i in 1:dim(cor2bw)[1]) {
+    if (cor2bw[i,2]>cor) break
   }
-  h
+  cor2bw[i,1]
 }
 
 bandwidth <- function(res,mask) { # second argument !!!!
   cxyz <- correlation(res,mask)
-  bwx <- smoothness(cxyz[1],dim(res)[1:3],0.5,0.01)
-  bwy <- smoothness(cxyz[2],dim(res)[1:3],0.5,0.01)
-  bwz <- smoothness(cxyz[3],dim(res)[1:3],0.5,0.01)
+  bwx <- smoothness(cxyz[1])
+  bwy <- smoothness(cxyz[2])
+  bwz <- smoothness(cxyz[3])
   meingauss<-gkernsm(array(rnorm(prod(dim(res)[1:3])),dim=dim(res)[1:3]),c(bwx,bwy,bwz))
   list(bwcorr=1/meingauss$kernsq,bw=c(bwx,bwy,bwz))
 }
