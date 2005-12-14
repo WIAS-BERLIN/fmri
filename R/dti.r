4edad2f18e9207d76e09aacaaf6b5f71b60e3850
stat <stat@090e7faf-2252-4674-a6b5-969773fc8662>
2005-12-14 14:51:07 +0000

    author: Karsten
    Added new dti module with 4 functions
    
    1. create.designmatrix.dti()
       creates the matrix of the linear model for ln(s(b)/s0) from the
       gradient b vectors and the b-value.
    2. calculate.lm.dti()
       estimate 6 tensor components from the linear model
       return, tensor elements, residualvarianz, xtx, and residuals (if
       needed)
    3. determine.eigenvalue()
       calculate field of eigenvalues from tensor field.
    4. anisotropy()
       calculate various anisotropy indices from the field of eigenvalues.
    
    This module is still under construction. So, neither names nor location of
    functions within the package are fixed. dti actually differs a lot from
    fmri. We add it to the fmri package, since the aws procedures needed are
    the same (cf. vaws3D). Note: a fucntion to determine the eigenvectors is
    still missing.
    
    
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/fmri-svn/trunk@43 090e7faf-2252-4674-a6b5-969773fc8662

---
+++ R/dti.r
@@ -0,0 +1,104 @@
+create.designmatrix.dti <- function(bvec, bvalue=1) {
+  dimension <- dim(bvec)[2] # should be 3
+  if (dimension != 3) {
+    warning("Error: gradient vectors do not have length 3")
+    return(invisible(NULL))
+  }
+  directions <- dim(bvec)[1] # number of measured directions
+
+  z <- matrix(0, directions, 6)
+
+  for (d in 1:directions) {
+    z[d,1] <- bvec[d,1]^2
+    z[d,2] <- bvec[d,2]^2
+    z[d,3] <- bvec[d,3]^2
+    z[d,4] <- 2*bvec[d,1]*bvec[d,2]
+    z[d,5] <- 2*bvec[d,1]*bvec[d,3]
+    z[d,6] <- 2*bvec[d,2]*bvec[d,3]
+    z[d,] <- bvalue*z[d,]
+  }
+  
+  z
+}
+
+calculate.lm.dti <- function(ttt,z,res=FALSE) {
+  svdresult <- svd(z)
+  u <- svdresult$u
+  v <- svdresult$v
+  vt <- t(v)
+  lambda1 <- diag(1/svdresult$d)
+  lambda2 <- diag(1/svdresult$d^2)
+  xtx <- v %*% lambda2 %*% vt
+  # now we have z = u lambda1^(-1) vt
+  cat("SVD done\n")
+  
+  # define some variables and make ttt a matrix
+  dy <- dim(ttt)
+  voxelcount <- prod(dy[1:3])
+  dim(ttt) <- c(prod(dy[1:3]),dy[4])
+  cat("definitions\n")
+  
+  # calculate the paramters and residuals for all voxels
+  beta <- ttt %*% u %*% lambda1 %*% vt
+  cat("parameters\n")
+  residuals <- ttt - beta %*% t(z)
+  cat("residuals\n")
+  b <- rep(1/dy[4],length=dy[4])
+  variance <- ((residuals^2 %*% b) * dim(z)[1] / (dim(z)[1]-dim(z)[2]))
+  dim(variance) <- c(dy[1:3])
+  cat("variance\n")
+
+  if (res) {
+    list(dt=beta,xtx=xtx,variance=variance,residuals=residuals)
+  } else {
+    list(dt=beta,xtx=xtx,variance=variance)    
+  }
+}
+  
+determine.eigenvalue <- function(diff) {
+
+  eigen <- array(0,dim=c(dim(diff)[1],dim(diff)[2],dim(diff)[3],3))
+  for (i in 1:dim(diff)[1]) {
+    cat(".")
+    for (j in 1:dim(diff)[2]) {
+      for (k in 1:dim(diff)[3]) {
+        tensor <- array(0,dim=c(3,3))
+        tensor[1,1] <- diff[i,j,k,1]
+        tensor[1,2] <- diff[i,j,k,4]
+        tensor[1,3] <- diff[i,j,k,5]
+        tensor[2,1] <- diff[i,j,k,4]
+        tensor[2,2] <- diff[i,j,k,2]
+        tensor[2,3] <- diff[i,j,k,6]
+        tensor[3,1] <- diff[i,j,k,5]
+        tensor[3,2] <- diff[i,j,k,6]
+        tensor[3,3] <- diff[i,j,k,3]
+        eigen[i,j,k,] <- eigen(tensor)$values
+      }
+    }
+  }
+
+  list(eigen=eigen)
+}
+
+anisotropy <- function(eigen) {
+  
+  fa <-
+    array(0,dim=c(dim(eigen)[1],dim(eigen)[2],dim(eigen)[3]))
+  ra <-
+    array(0,dim=c(dim(eigen)[1],dim(eigen)[2],dim(eigen)[3]))
+  trc <-
+    array(0,dim=c(dim(eigen)[1],dim(eigen)[2],dim(eigen)[3]))
+  
+  for (i in 1:dim(eigen)[1]) {
+    cat(".")
+    for (j in 1:dim(eigen)[2]) {
+      for (k in 1:dim(eigen)[3]) {
+        trc[i,j,k] <- mean(eigen[i,j,k,])
+        fa[i,j,k] <- sqrt(3*sum((eigen[i,j,k,]-trc[i,j,k])^2))/sqrt(2*(sum(eigen[i,j,k,]^2)))
+        ra[i,j,k] <- sqrt(sum((eigen[i,j,k,]-trc[i,j,k])^2))/sqrt(3*trc[i,j,k])
+      }
+    }
+  }
+
+  list(fa=fa,ra=ra, trace=trc)
+}
