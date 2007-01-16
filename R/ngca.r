43a61a239b755d9bc6c30941918638820b2c9423
stat <stat@090e7faf-2252-4674-a6b5-969773fc8662>
2007-01-16 08:15:33 +0000

    function ngca has a new argument seigen that forces a dimension reduction to PCA components that acoount for 100*seigen percent of the variability in the data. This automatically removes effects of singular Covariance matrices.
    Joerg
    
    
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/fmri-svn/trunk@133 090e7faf-2252-4674-a6b5-969773fc8662

--- R/ngca.r
+++ R/ngca.r
@@ -1,4 +1,4 @@
-ngca <- function(x,L=1000,T=10,m=3,eps=1.5){
+ngca <- function(x,L=1000,T=10,m=3,eps=1.5,seigen=.99){
 #
 #  NGCA algorithm 
 #
@@ -12,7 +12,9 @@ xmean <- apply(x,2,mean)
 xvar <- var(x)
 y <- sweep(x,2,xmean)
 z <- svd(xvar)
-y <- y%*%z$u%*%diag(z$d^(-.5))
+dd <- sum(cumsum(z$d)<seigen*sum(z$d))
+cat("Dimension reduced to:",dd,"\n")
+y <- y%*%z$u%*%diag(z$d^(-.5))[,1:dd]
 y <- t(y)
 #
 #  thats the standardized version of x
@@ -25,24 +27,24 @@ s[,4] <- seq(0,4,length=L)
 #
 #   now fast ICA
 #
-omega <- matrix(rnorm(4*L*d),d,L*4)
+omega <- matrix(rnorm(4*L*dd),dd,L*4)
 omega <- sweep(omega,2,sqrt(apply(omega^2,2,sum)),"/")
 fz <- .Fortran("fastica",
               as.double(y),
               as.double(omega),
-              as.integer(d),
+              as.integer(dd),
               as.integer(n),
               as.integer(L),
               as.integer(T),
-              double(d),
-              v=double(d*L*4),
+              double(dd),
+              v=double(dd*L*4),
               normv=double(L*4),
               as.double(s),
               DUP=FALSE,
               PACKAGE="fmri")[c("v","normv")]
-dim(fz$v) <- c(d,4*L)
+dim(fz$v) <- c(dd,4*L)
 fz$v <- t(fz$v[,fz$normv>eps])
 jhat <- prcomp(fz$v)
-ihat <- z$u%*%jhat$rotation[,1:m]
+ihat <- z$u[,1:dd]%*%jhat$rotation[,1:m]
 list(ihat=ihat,sdev=jhat$sdev[1:m])
 }
