c5e68f7d1eb914b4c78077cd42cc3c03ddcc8e9c
stat <stat@090e7faf-2252-4674-a6b5-969773fc8662>
2007-03-28 13:43:57 +0000

    revised FORTRAN subroutine corr new version 1.1-4
    Joerg
    
    
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/fmri-svn/trunk@152 090e7faf-2252-4674-a6b5-969773fc8662

--- src/misc.f
+++ src/misc.f
@@ -4,101 +4,128 @@
       integer n1,n2,n3,nv
       real*8 scorr(3),res(n1,n2,n3,nv)
       logical mask(n1,n2,n3)
-      real*8 z,z2,resi,vrm
+      real*8 z,z2,resi,resip1,vrm,zk,zcorr
       integer i1,i2,i3,i4,k
       
       scorr(1)=0.d0
       scorr(2)=0.d0
       scorr(3)=0.d0
+C  correlation in x
       z=0.d0
       z2=0.d0
+      zcorr=0.d0
       k=0
-
       do i1=1,n1
          do i2=1,n2
             do i3=1,n3
-               if (.not.mask(i1,i2,i3)) CYCLE
+               if (.not.(mask(i1,i2,i3).and.mask(i1+1,i2,i3))) CYCLE
                do i4=1,nv
                   resi=res(i1,i2,i3,i4)
-                  if (resi.eq.0.d0) CYCLE
+                  resip1=res(i1+1,i2,i3,i4)
+                  if (resi.eq.0.d0.or.resip1.eq.0.d0) CYCLE
                   z=z+resi
                   z2=z2+resi*resi
+                  zcorr=zcorr+resi*resip1
                   k=k+1
                enddo
             enddo
          enddo
       enddo
-      if (k.gt.0) then 
-         z=z/k
-         vrm=z2/k-(z*z)
-         k=0
-         z=0.d0
-         do i1=1,n1-1
-            do i2=1,n2
-               do i3=1,n3
-                  if (.not.(mask(i1,i2,i3).and.mask(i1+1,i2,i3))) CYCLE
-                  do i4=1,nv
-                     resi=res(i1,i2,i3,i4)*res(i1+1,i2,i3,i4)
-                     if (resi.eq.0.d0) CYCLE
-                     z=z+resi
-                     k=k+1
-                  enddo
-               enddo
-            enddo
-         enddo
-         if (k.gt.0) THEN
-             scorr(1)=z/k/vrm
-         ELSE
-             scorr(1)=0.d0
-             call intpr("All zero residuals x",20,scorr(1),1)
-         END IF
-         k=0
-         z=0.d0
-         do i1=1,n1
-            do i2=1,n2-1
-               do i3=1,n3
-                  if (.not.(mask(i1,i2,i3).and.mask(i1,i2+1,i3))) CYCLE
-                  do i4=1,nv
-                     resi=res(i1,i2,i3,i4)*res(i1,i2+1,i3,i4)
-                     if (resi.eq.0.d0) CYCLE
-                     z=z+resi
-                     k=k+1
-                  enddo
-               enddo
-            enddo
-         enddo
-         if (k.gt.0) THEN
-             scorr(2)=z/k/vrm
-         ELSE
-             scorr(2)=0.d0
-             call intpr("All zero residuals y",20,scorr(2),1)
-         END IF
-         k=0
-         z=0.d0
-         do i1=1,n1
-            do i2=1,n2
-               do i3=1,n3-1
-                  if (.not.(mask(i1,i2,i3).and.mask(i1,i2,i3+1))) CYCLE
-                  do i4=1,nv
-                     resi=res(i1,i2,i3,i4)*res(i1,i2,i3+1,i4)
-                     if (resi.eq.0.d0) CYCLE
-                     z=z+resi
-                     k=k+1
-                  enddo
-               enddo
-            enddo
-         enddo
-         if (k.gt.0) THEN
-             scorr(3)=z/k/vrm
-         ELSE
-             scorr(3)=0.d0
-             call intpr("All zero residuals z",20,scorr(3),1)
-         END IF
+      if (k.gt.0) THEN
+         zk=k
+         z=z/zk
+         vrm=(z2-zk*(z*z))/(zk-1.d0)
+         scorr(1)=zcorr/zk/vrm
       ELSE
          scorr(1)=0.d0
+         call dblepr("All zero residuals x",20,scorr(1),1)
+      END IF
+      if(scorr(1).gt.1.d0-1.d-8) THEN
+         call dblepr("scorr(1) was",12,scorr(1),1)
+         scorr(1) = 1.d0-1.d-8
+         call dblepr("scorr(1) reset to",17,scorr(1),1)
+      ELSE IF (scorr(1).lt.-1.d0+1.d-8) THEN
+         call dblepr("scorr(1) was",12,scorr(1),1)
+         scorr(1) = 1.d0-1.d-8
+         call dblepr("scorr(1) reset to",17,scorr(1),1)
+      END IF
+C  correlation in y
+      z=0.d0
+      z2=0.d0
+      zcorr=0.d0
+      k=0
+      do i1=1,n1
+         do i2=1,n2
+            do i3=1,n3
+               if (.not.(mask(i1,i2,i3).and.mask(i1,i2+1,i3))) CYCLE
+               do i4=1,nv
+                  resi=res(i1,i2,i3,i4)
+                  resip1=res(i1,i2+1,i3,i4)
+                  if (resi.eq.0.d0.or.resip1.eq.0.d0) CYCLE
+                  z=z+resi
+                  z2=z2+resi*resi
+                  zcorr=zcorr+resi*resip1
+                  k=k+1
+               enddo
+            enddo
+         enddo
+      enddo
+      if (k.gt.0) THEN
+         zk=k
+         z=z/zk
+         vrm=(z2-zk*(z*z))/(zk-1.d0)
+         scorr(2)=zcorr/zk/vrm
+      ELSE
          scorr(2)=0.d0
+         call dblepr("All zero residuals y",20,scorr(2),1)
+      END IF
+      if(scorr(2).gt.1.d0-1.d-8) THEN
+         call dblepr("scorr(2) was",12,scorr(2),1)
+         scorr(2) = 1.d0-1.d-8
+         call dblepr("scorr(2) reset to",17,scorr(2),1)
+      ELSE IF (scorr(2).lt.-1.d0+1.d-8) THEN
+         call dblepr("scorr(2) was",12,scorr(2),1)
+         scorr(1) = 1.d0-1.d-8
+         call dblepr("scorr(2) reset to",17,scorr(2),1)
+      END IF
+C  correlation in z
+      z=0.d0
+      z2=0.d0
+      zcorr=0.d0
+      k=0
+      do i1=1,n1
+         do i2=1,n2
+            do i3=1,n3
+               if (.not.(mask(i1,i2,i3).and.mask(i1,i2,i3+1))) CYCLE
+               do i4=1,nv
+                  resi=res(i1,i2,i3,i4)
+                  resip1=res(i1,i2,i3+1,i4)
+                  if (resi.eq.0.d0.or.resip1.eq.0.d0) CYCLE
+                  z=z+resi
+                  z2=z2+resi*resi
+                  zcorr=zcorr+resi*resip1
+                  k=k+1
+               enddo
+            enddo
+         enddo
+      enddo
+      if (k.gt.0) THEN
+         zk=k
+         z=z/zk
+         vrm=(z2-zk*(z*z))/(zk-1.d0)
+         scorr(3)=zcorr/zk/vrm
+      ELSE
          scorr(3)=0.d0
-         call intpr("All zero residuals xyz",22,scorr,3)
-      endif
+         call dblepr("All zero residuals x",20,scorr(3),1)
+      END IF
+      if(scorr(3).gt.1.d0-1.d-8) THEN
+         call dblepr("scorr(3) was",12,scorr(3),1)
+         scorr(3) = 1.d0-1.d-8
+         call dblepr("scorr(3) reset to",17,scorr(3),1)
+      ELSE IF (scorr(3).lt.-1.d0+1.d-8) THEN
+         call dblepr("scorr(3) was",12,scorr(3),1)
+         scorr(3) = 1.d0-1.d-8
+         call dblepr("scorr(3) reset to",17,scorr(3),1)
+      END IF
       return
       end
