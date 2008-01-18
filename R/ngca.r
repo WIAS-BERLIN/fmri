70eaeb72b58158fe84c3ef79d9a1258f45814381
stat <stat@090e7faf-2252-4674-a6b5-969773fc8662>
2008-01-18 16:59:46 +0000

    new function fmriica requires package fastICA to be installed works on fmridata - objects takes parameters from fastICA via ...
    untested
    
    Joerg
    
    
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/fmri-svn/trunk@204 090e7faf-2252-4674-a6b5-969773fc8662

--- R/ngca.r
+++ R/ngca.r
@@ -1,4 +1,4 @@
-ngca <- function(data,L=1000,T=10,m=3,eps=1.5,npca=min(dim(x)[2],dim(x)[1])-1,method="spatial",sweepmean=NULL,keepv=FALSE){
+ngca <- function(data,L=1000,T=10,m=3,eps=1.5,npca=min(dim(x)[2],dim(x)[1]),method="spatial",sweepmean=NULL,keepv=FALSE){
 #
 #  NGCA algorithm  for fMRI
 #  x should be either a fMRI object or a matrix 
@@ -29,7 +29,7 @@ if(length(mask)==1) mask <- rep(mask,n)
 x <- x[mask,]
 n <- sum(mask)
 if(is.null(sweepmean)) sweepmean <- switch(method,"spatial"="temporal","temporal"="spatial")
-if(is.null(npca)||npca >= min(d,n)) npca <- min(d,n)-1
+if(is.null(npca)||npca >= min(d,n)) npca <- min(d,n)
 x <- switch(sweepmean,"none"=x,"global"=x - mean(x),"spatial"=sweep(x,2,apply(x,1,mean)),
             "temporal"=sweep(x,2,apply(x,2,mean)))
 if(method=="spatial"){
@@ -41,7 +41,9 @@ svdx <- svd(x,nu=0,nv=npca)
 #xvar <- var(x)
 #z <- svd(xvar)
 cat("Dimension reduced to:",npca,"\n")
-y <- t(x%*%svdx$v%*%diag(1/svdx$d[1:npca]))
+svdxd <- svdxdinv <- abs(svdx$d[1:npca])
+svdxdinv[svdxdinv>0] <- 1/svdxdinv[svdxdinv>0]
+y <- t(x%*%svdx$v%*%diag(svdxdinv))
 #
 #  thats the standardized version of x
 #
@@ -73,7 +75,7 @@ normv <- fz$normv
 dim(v) <- c(npca,4*L)
 v <- t(v[,normv>eps])
 jhat <- prcomp(v)
-ihat <- svdx$v%*%diag(svdx$d[1:npca])%*%jhat$rotation[,1:m]
+ihat <- svdx$v%*%diag(svdxd)%*%jhat$rotation[,1:m]
 xhat <- x%*%ihat
 if(fmriobj){
 if(method=="spatial"){
@@ -101,3 +103,55 @@ class(z) <- "ngca"
 }
 z
 }
+
+fmriica <- function(data,m=3,method="spatial",...){
+#
+#  NGCA algorithm  for fMRI
+#  x should be either a fMRI object or a matrix 
+#
+if(!require(fastICA)) stop("Please install package fastICA from CRAN")
+if(all(class(data)=="fmridata")) {
+   x <- extract.data(data)
+   mask <- data$mask
+}  else {
+   warning("data has incompatible class argument")
+   return(data)
+}
+#
+#  x - data matrix  (Nxd)
+#
+set.seed(1)
+xdim <- dim(x)
+lxdim <- length(xdim)
+d <- xdim[lxdim]
+n <- nn <- prod(xdim[1:(lxdim-1)])
+dim(x) <- c(n,d)
+mask <- as.vector(mask)
+if(length(mask)==1) mask <- rep(mask,n)
+x <- x[mask,]
+n <- sum(mask)
+if(method=="spatial"){
+x <- t(x)
+d <- dim(x)[2]
+n <- dim(x)[1]
+}
+#
+#   now fast ICA
+#
+z <- fastICA(z,m,method="C",...)
+if(method=="spatial"){
+z <- matrix(0,nn,m)
+z[mask,]<-z$S
+ihat <- array(z,c(xdim[1:3],m))
+xhat <- z$A
+} else {
+z <- matrix(0,nn,m)
+z[mask,]<-z$A
+xhat <- array(z,c(xdim[1:3],m))
+ihat <- z$S
+}
+z <- list(ihat=ihat,xhat=xhat)
+class(z) <- "fmringca"
+z
+}
+
