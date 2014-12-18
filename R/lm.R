e07130e46afa785b3b874b80443a9f67b633c27f
stat <stat@090e7faf-2252-4674-a6b5-969773fc8662>
2014-12-18 14:18:12 +0000

    - formatting in lm.R - Rbuildignore file for correct build
    k
    
    
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/fmri-svn/trunk@342 090e7faf-2252-4674-a6b5-969773fc8662

--- R/lm.R
+++ R/lm.R
@@ -1,14 +1,14 @@
 ### create expected BOLD convoluted with HRF
 fmri.stimulus <- function(scans = 1,
-                         onsets = c(1),
-                         durations = c(1),
-                         TR = 2,
-                         times = FALSE,
-                         type = c("canonical", "gamma", "boxcar", "user"),
-                         par = NULL,
-                         scale = 10,
-                         hrf = NULL,
-                         verbose = FALSE) {
+                          onsets = c(1),
+                          durations = c(1),
+                          TR = 2,
+                          times = FALSE,
+                          type = c("canonical", "gamma", "boxcar", "user"),
+                          par = NULL,
+                          scale = 10,
+                          hrf = NULL,
+                          verbose = FALSE) {
   
   ## match the type of HRF function to the defaults
   type <- match.arg(type)
@@ -90,8 +90,8 @@ fmri.stimulus <- function(scans = 1,
 
 ## OLD VERSION OF THE FUNCTION!
 fmri.stimulus.old <- function(scans=1 ,onsets=c(1) ,durations=c(1),
-  rt=3, times=NULL, mean=TRUE, a1 = 6, a2 = 12, b1 = 0.9, b2 = 0.9, cc = 0.35) {
-
+                              rt=3, times=NULL, mean=TRUE, a1 = 6, a2 = 12, b1 = 0.9, b2 = 0.9, cc = 0.35) {
+  
   mygamma <- function(x, a1, a2, b1, b2, c) {
     d1 <- a1 * b1
     d2 <- a2 * b2
@@ -100,9 +100,9 @@ fmri.stimulus.old <- function(scans=1 ,onsets=c(1) ,durations=c(1),
     res <- c1 * exp(-(x-d1)/b1) - c2 * exp(-(x-d2)/b2)
     res
   }
-
-
-
+  
+  
+  
   if (is.null(times)) {
     scale <- 1
   } else {
@@ -127,7 +127,7 @@ fmri.stimulus.old <- function(scans=1 ,onsets=c(1) ,durations=c(1),
     }
   }
   stimulus <- c(rep(0,20*scale),stimulus,rep(0,20*scale))
-#  just fill with zeros to avoid bounding effects in convolve
+  #  just fill with zeros to avoid bounding effects in convolve
   hrf <- convolve(stimulus,mygamma(((40*scale)+scans):1, a1, a2, b1/rt, b2/rt, cc))/scale
   hrf <- hrf[-(1:(20*scale))][1:scans]
   hrf <- hrf[unique((scale:scans)%/%scale)*scale]
@@ -207,22 +207,22 @@ fmri.design <- function(stimulus,
 fmri.design.old <- function(hrf, order=2) {
   stimuli <- dim(hrf)[2]
   scans <- dim(hrf)[1]
-
+  
   z <- matrix(0, scans, stimuli+order+1)
-
+  
   for (i in 1:stimuli) {
     z[,i] <- hrf[,i]
   }
-
+  
   ortho <- matrix(0, stimuli, stimuli)
   for (i in 1:stimuli) {
     for (j in 1:stimuli) {
       ortho[i,j] <- z[,i]%*%z[,j]
     }
   }
-
+  
   z[,stimuli+1] <- 1
-
+  
   if (order != 0) {
     for (i in (stimuli+2):(stimuli+order+1)) {
       z[,i] <- (1:scans)^(i-stimuli-1)
@@ -419,7 +419,7 @@ fmri.lm <- function(ds,
                    as.integer(lags[3]),
                    PACKAGE = "fmri")$scorr
   dim(corr) <- lags                     
-
+  
   ## "compress" the residuals
   scale <- max(abs(range(residuals)))/32767
   residuals <- writeBin(as.integer(residuals/scale), raw(), 2)
@@ -487,7 +487,7 @@ fmri.lm <- function(ds,
   attr(result, "design") <- z
   attr(result, "white") <- white
   attr(result, "residuals") <- !is.null(scale)
-    
+  
   if (verbose) cat("fmri.lm: exiting function at", format(Sys.time()), "\n")
   
   invisible(result)
@@ -496,15 +496,15 @@ fmri.lm <- function(ds,
 
 fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(1),vvector=c(1),keep="all") {
   cat("fmri.lm: entering function\n")
-
+  
   hmax <- 3.52
   if (!class(data) == "fmridata") {
     warning("fmri.lm: data not of class <fmridata>. Try to proceed but strange things may happen")
   }
   args <- sys.call()
-
+  
   ttt <- extract.data(data)
-
+  
   if (length(dim(ttt)) != 4) {
     stop("Hmmmm, this does not seem to be a fMRI time series. I better stop executing! Sorry!\n")
   }
@@ -521,12 +521,12 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
     
     a
   }
-
+  
   # first consider contrast vector! NO test whether it is real contrast!!
   if (length(contrast) <= dim(z)[2]) contrast <- c(contrast,rep(0,dim(z)[2]-length(contrast)))
   length(contrast) <- dim(z)[2]
   if ((length(vvector) < dim(z)[2]) && (length(vvector) > 1)) vvector <- c(vvector,rep(0,dim(z)[2]-length(vvector)))
-
+  
   # first get the SVD for the design matrix
   svdresult <- svd(z)
   u <- svdresult$u
@@ -536,7 +536,7 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
   lambda2 <- diag(1/svdresult$d^2)
   xtx <- v %*% lambda2 %*% vt
   # now we have z = u lambda1^(-1) vt
-
+  
   # define some variables and make ttt a matrix
   dy <- dim(ttt)
   voxelcount <- prod(dy[1:3])
@@ -544,7 +544,7 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
   arfactor <- rep(0,length=prod(dy[1:3]))
   variance <- rep(0,length=prod(dy[1:3]))
   if (length(vvector) > 1) variancem <- array(0,c(dy[1:3],length(vvector)^2))
-
+  
   # calculate matrix R for bias correction in correlation coefficient
   # estimate
   R <- diag(1,dy[4]) - u %*% t(u)
@@ -559,7 +559,7 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
   # calculate the paramters and residuals for all voxels
   beta <- ttt %*% u %*% lambda1 %*% vt
   residuals <- ttt - beta %*% t(z)
-
+  
   # actype == "smooth" ... calc AC, smooth AC, calc prewhitened model
   # actype == "accalc" ... calc AC, calc prewhitened model
   # actype == "ac"     ... calc AC only
@@ -583,15 +583,15 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
       } else {
         arfactor[i] <- 0
       }
-
+      
       if (arfactor[i] >= 1) arfactor[i] <- 0.999
       
       ### this method does have a bias!
-#      if (sum(abs(residuals[i,]))) {
-#        arfactor[i] <- residuals[i,2:dim(z)[1]] %*% residuals[i,1:(dim(z)[1]-1)] / residuals[i,] %*% residuals[i,]
-#      } else {
-#        arfactor[i] <- 0 # avoid NaN
-#      }
+      #      if (sum(abs(residuals[i,]))) {
+      #        arfactor[i] <- residuals[i,2:dim(z)[1]] %*% residuals[i,1:(dim(z)[1]-1)] / residuals[i,] %*% residuals[i,]
+      #      } else {
+      #        arfactor[i] <- 0 # avoid NaN
+      #      }
       ### leave it for historical reasons
       
     }
@@ -602,12 +602,12 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
       dim(arfactor) <- dy[1:3]
       # now smooth (if actype is such) with AWS
       hinit <- 1
-#      arfactor <- gkernsm(arfactor,rep(hmax,3)*0.42445)$gkernsm
+      #      arfactor <- gkernsm(arfactor,rep(hmax,3)*0.42445)$gkernsm
       arfactor <- smooth3D(arfactor,lkern="Gaussian",hmax=hmax,wghts=data$weights,mask=data$mask)
       dim(arfactor) <- voxelcount
       cat("fmri.lm: finished\n")
     }
-
+    
     if ((actype == "smooth") || (actype == "accalc")) {
       progress = 0
       cat("fmri.lm: re-calculating linear model with prewithened data\n")
@@ -623,7 +623,7 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
           progress = progress + 10
         }
         indar <- as.logical((arfactor > arlist[i]) * (arfactor <=
-                                                      arlist[i+1]))
+                                                        arlist[i+1]))
         if(sum(indar)>0){
           a <- create.arcorrection(dy[4],mean(arlist[i:(i+1)])) # create prewhitening matrix
           zprime <- a %*% as.matrix(z)
@@ -639,14 +639,14 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
         }
         gc()
       }
-#  prewhitened residuals don't have zero mean, therefore sweep mean over time from them
+      #  prewhitened residuals don't have zero mean, therefore sweep mean over time from them
       residuals <- .Fortran("sweepm",residuals=as.double(t(residuals)),
-                                     as.logical(data$mask),
-                                     as.integer(dy[1]),
-                                     as.integer(dy[2]),
-                                     as.integer(dy[3]),
-                                     as.integer(dy[4]),
-                                     PACKAGE="fmri",DUP=TRUE)$residuals
+                            as.logical(data$mask),
+                            as.integer(dy[1]),
+                            as.integer(dy[2]),
+                            as.integer(dy[3]),
+                            as.integer(dy[4]),
+                            PACKAGE="fmri",DUP=TRUE)$residuals
       dim(residuals) <- c(dy[4],prod(dy[1:3]))
       residuals <- t(residuals)
       b <- rep(1/dy[4],length=dy[4])
@@ -660,7 +660,7 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
       variance <- ((residuals^2 %*% b) * dy[4] / (dy[4]-dim(z)[2])) %*% cxtx
     }
   } else { # actype == "noac"
-                                        # estimate variance, add more paramters if needed
+    # estimate variance, add more paramters if needed
     b <- rep(1/dy[4],length=dy[4])
     cxtx <- t(contrast) %*% xtx %*% contrast
     if (length(vvector) > 1) variancem <- ((residuals^2 %*% b) * dim(z)[1] / (dim(z)[1]-dim(z)[2])) %*% as.vector(xtx[as.logical(vvector),as.logical(vvector)])
@@ -674,37 +674,37 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
   if (length(vvector) > 1) dim(variancem) <- c(dy[1:3],sum(as.logical(vvector)),sum(as.logical(vvector)))
   dim(arfactor) <- dy[1:3]
   residuals <- t(residuals)
-
+  
   cat("fmri.lm: calculating spatial correlation\n")
-
+  
   lags <- c(5,5,3)
   corr <- .Fortran("mcorr",as.double(residuals),
-                     as.logical(data$mask),
-                     as.integer(dy[1]),
-                     as.integer(dy[2]),
-                     as.integer(dy[3]),
-                     as.integer(dy[4]),
-                     scorr=double(prod(lags)),
-                     as.integer(lags[1]),
-                     as.integer(lags[2]),
-                     as.integer(lags[3]),
-                     PACKAGE="fmri",DUP=TRUE)$scorr
+                   as.logical(data$mask),
+                   as.integer(dy[1]),
+                   as.integer(dy[2]),
+                   as.integer(dy[3]),
+                   as.integer(dy[4]),
+                   scorr=double(prod(lags)),
+                   as.integer(lags[1]),
+                   as.integer(lags[2]),
+                   as.integer(lags[3]),
+                   PACKAGE="fmri",DUP=TRUE)$scorr
   dim(corr) <- lags                     
   scale <- NULL
   if(keep=="all"){
-     qscale <- range(residuals)
-     scale <- max(abs(qscale))/32767
-     residuals <- writeBin(as.integer(residuals/scale),raw(),2)
+    qscale <- range(residuals)
+    scale <- max(abs(qscale))/32767
+    residuals <- writeBin(as.integer(residuals/scale),raw(),2)
   }
   bw <- optim(c(2,2,2),corrrisk,method="L-BFGS-B",lower=c(.59,.59,.59),upper=c(10,10,10),lag=lags,data=corr)$par  
   bw[bw<=.6] <- 0
   if( (max(bw) > 2.5 ) || (corr[lags[1],1,1]+corr[1,lags[2],1]+corr[1,1,lags[3]] >0.5) ) warning(paste("Local smoothness characterized by large bandwidth ",bw," check residuals for structure",collapse=","))
   rxyz <- c(resel(1,bw[1]), resel(1,bw[2]), resel(1,bw[3]))
   dim(rxyz) <- c(1,3)
-
+  
   variance[variance == 0] <- 1e20
   if (length(vvector) > 1) variancem[variancem == 0] <- 1e20
-
+  
   if (length(vvector) > 1) {
     cbeta <- beta[,,,1:sum(vvector>0)]
     vwghts <- numeric(sum(vvector>0))
@@ -712,7 +712,7 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
   } else {
     vwghts <- 1
   }
-
+  
   cat("fmri.lm: determining df: ")
   if (actype == "smooth") {
     white <- 1
@@ -724,27 +724,27 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
   cx <- u %*% lambda1 %*% vt %*% contrast
   tau1 <- sum(cx[-1] * cx[-length(cx)]) / sum(cx * cx)
   df <- switch(white,abs(diff(dim(z))) / (1 + 2*(1 + 2 * prod(hmax/bw)^0.667)^(-1.5) * tau1^2) ,
-                     abs(diff(dim(z))) / (1 + 2* tau1^2)  ,
-                     abs(diff(dim(z))) )
+               abs(diff(dim(z))) / (1 + 2* tau1^2)  ,
+               abs(diff(dim(z))) )
   cat(df,"\n")
-
+  
   cat("fmri.lm: exiting function\n")
   
   if (keep == "all") {
     result <- list(beta = beta, cbeta = cbeta, var = variance, res =
-              residuals, arfactor = arfactor, rxyz = rxyz, scorr = corr, weights =
-              data$weights, vwghts = vwghts, mask=data$mask, dim =
-              data$dim, hrf = z %*% contrast, resscale=scale, bw=bw, df=df, call=args)
+                     residuals, arfactor = arfactor, rxyz = rxyz, scorr = corr, weights =
+                     data$weights, vwghts = vwghts, mask=data$mask, dim =
+                     data$dim, hrf = z %*% contrast, resscale=scale, bw=bw, df=df, call=args)
   } else {
     result <- list(cbeta = cbeta, var = variance, rxyz = rxyz, scorr = corr, weights =
-              data$weights, vwghts = vwghts, mask=data$mask, dim = data$dim, 
-              hrf = z %*% contrast, res=NULL, resscale=NULL, bw=bw, df=df, call=args)
+                     data$weights, vwghts = vwghts, mask=data$mask, dim = data$dim, 
+                   hrf = z %*% contrast, res=NULL, resscale=NULL, bw=bw, df=df, call=args)
   }
-
+  
   if (length(vvector) > 1) {
     result$varm <- variancem
   }
-
+  
   result$roixa <- data$roixa
   result$roixe <- data$roixe
   result$roiya <- data$roiya
@@ -755,13 +755,13 @@ fmri.lm.old <- function(data,z,actype="smooth",vtype="var",step=0.01,contrast=c(
   result$header <- data$header
   result$format <- data$format
   result$dim0 <- data$dim0
-    class(result) <- c("fmridata","fmrispm")
-
+  class(result) <- c("fmridata","fmrispm")
+  
   attr(result, "file") <- attr(data, "file")
   attr(result, "design") <- z
   attr(result, "white") <- white
   attr(result, "residuals") <- !is.null(scale)
-    
+  
   invisible(result)
 }
 
